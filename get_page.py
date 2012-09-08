39b8d9218baa1799d1c96ba9c1447316549706ba
ashish <aitashish173@gmail.com>
2012-09-08 12:56:05 +0530

    editing get_page.py

--- get_page.py
+++ get_page.py
@@ -1,6 +1,18 @@
+#!usr/bin/python
+import re
+import urllib2
+
+def get_page(url):
+	f = urllib2.urlopen(url) # page handler
+	content = f.read()   # reading the source code of webpage.
+	return content
+	
+
 def main():
 	print 'hello world!!'
-
+	url = raw_input("enter the url")
+	content =  get_page(url)
+	
 
 if __name__ == "__main__":
 	main()
